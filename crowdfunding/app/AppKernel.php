ccfa4c4b424d07319859ad3ef01ea646baa5e637
sanji69 <axel.aubry69@gmail.com>
2018-05-31 16:11:09 +0200

    'fos_suser'

--- crowdfunding/app/AppKernel.php
+++ crowdfunding/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
 
             //manual install
              new FOS\UserBundle\FOSUserBundle(),
+             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
