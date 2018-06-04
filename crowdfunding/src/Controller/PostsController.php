0ac3b97759b64e8e0df18937575c7e31aaed3d3e
sanji69 <axel.aubry69@gmail.com>
2018-06-04 15:00:45 +0200

    ajout_pluging_encore

--- crowdfunding/src/Controller/PostsController.php
+++ crowdfunding/src/Controller/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,10 +17,17 @@ class PostsController extends Controller
     /**
      * @Route("/posts", name="posts")
      */
-    public function index(Environment $twig, RegistryInterface $doctrine, FormFactoryInterface $formFactory)
+    public function index(Request $request, Environment $twig, RegistryInterface $doctrine, FormFactoryInterface $formFactory)
     {
         $posts = $doctrine->getRepository(Post::class)->findAll();
         $form = $formFactory->createBuilder(PostType::class, $posts)->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $doctrine->getEntityManager()->flush();
+        }
 
         return new Response($twig->render('posts/index.html.twig',[
             'posts' => $posts,
--- crowdfunding/src/Entity/Post.php
+++ crowdfunding/src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
@@ -18,6 +19,7 @@ class Post
 
     /**
      * @ORM\Column(type="string")
+     * @Assert\Length(min=200)
      */
     private $title;
 
