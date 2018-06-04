79a332bbfa8a15e29c9fff1bf3fc1820b2f63d17
sanji69 <axel.aubry69@gmail.com>
2018-06-04 14:22:50 +0200

    maj

--- crowdfunding/src/Controller/PostsController.php
+++ crowdfunding/src/Controller/PostsController.php
@@ -3,19 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class PostsController extends Controller
 {
     /**
      * @Route("/posts", name="posts")
      */
-    public function index(RegistryInterface $postRepository)
+    public function index(Environment $twig, RegistryInterface $doctrine, FormFactoryInterface $formFactory)
     {
-        $posts = $postRepository->getRepository(Post::class)->findAll();
-        return $this->render('posts/index.html.twig', compact('posts'));
+        $posts = $doctrine->getRepository(Post::class)->findAll();
+        $form = $formFactory->createBuilder(PostType::class, $posts)->getForm();
+
+        return new Response($twig->render('posts/index.html.twig',[
+            'posts' => $posts,
+            'form' => $form->createView()
+        ]));
     }
 }
---
+++ crowdfunding/src/Form/PostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+//use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('publish', CheckboxType::class, ['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
