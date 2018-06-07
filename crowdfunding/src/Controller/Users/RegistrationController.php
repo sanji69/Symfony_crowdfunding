99f4ddf298a69133802f070777d81caf340f550d
sanji69 <axel.aubry69@gmail.com>
2018-06-07 16:09:22 +0200

    log_reg_end

--- crowdfunding/src/Controller/Users/RegistrationController.php
+++ crowdfunding/src/Controller/Users/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Users;
 
 use App\Form\UsersType;
 use App\Entity\Users;
@@ -8,42 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class UsersController extends Controller
+class RegistrationController extends Controller
 {
-    /**
-     * La route pour se deconnecter. "intercepté par symfony"
-     *
-     * @Route("/logout", name="security_logout")
-     */
-    public function logout(): void
-    {
-        throw new \Exception('This should never be reached!');
-    }
 
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/register", name="user_registration", methods={"GET", "POST"})
      */
-    public function login(AuthenticationUtils $helper): Response
-    {
-        return $this->render('users/login.html.twig', [
-            // dernier username saisi (si il y en a un)
-            'last_username' => $helper->getLastUsername(),
-            // La derniere erreur de connexion (si il y en a une)
-            'error' => $helper->getLastAuthenticationError(),
-        ]);
-    }
-
-    /**
-    * @Route("/register", name="user_registration", methods={"GET", "POST"})
-    */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         // mise en place du formulaire
         $user = new Users();
-//        $user->setRoles(['ROLE_USER']);
+        $user->setRoles(['ROLE_USER']);
         $form = $this->createForm(UsersType::class, $user);
 
         // preparer la requete
@@ -62,12 +38,12 @@ class UsersController extends Controller
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('security_login"');
+            return $this->redirectToRoute('security_login');
         }
 
         return $this->render(
-        'users/register.html.twig',
-        array('form' => $form->createView())
+            'users/register.html.twig',
+            array('form' => $form->createView())
         );
     }
 
