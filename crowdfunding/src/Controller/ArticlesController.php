890fa479a621359f7038949b01fdfacb05580933
sanji69 <axel.aubry69@gmail.com>
2018-06-07 12:35:46 +0200

    route_index_admin

--- crowdfunding/src/Controller/ArticlesController.php
+++ crowdfunding/src/Controller/ArticlesController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticlesController extends Controller
 {
+
     /**
-     * @Route("/", name="articles")
-     */
+    * @Route("/", name="index")
+    */
     public function index()
     {
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
-        ]);
+        return $this->render('articles/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('dashboard/dashboard.html.twig');
     }
 }
