bc6b2e837f804dbfac8fc2e6c58e82d31159a8b7
sanji69 <axel.aubry69@gmail.com>
2018-06-11 11:16:26 +0200

    data_fixture_articles

--- crowdfunding/src/DataFixtures/AppFixtures.php
+++ crowdfunding/src/DataFixtures/AppFixtures.php
@@ -3,48 +3,78 @@
 namespace App\DataFixtures;
 
 use App\Entity\Users;
+USE App\Entity\Articles;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-private $passwordEncoder;
+    private $passwordEncoder;
 
-public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-{
-$this->passwordEncoder = $passwordEncoder;
-}
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
-public function load(ObjectManager $manager)
-{
-foreach ($this->getUserData() as [$username, $lastname, $firstname, $password, $email, $roles]) {
-$user = new Users();
-$user->setUsername($username);
-$user->setLastname($lastname);
-$user->setFirstname($firstname);
-$user->setPassword($this->passwordEncoder->encodePassword($user, $password));
-$user->setEmail($email);
-$token = bin2hex(random_bytes(100));
-$user->setToken($token);
-$user->setRoles($roles);
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$username, $lastname, $firstname, $password, $email, $roles]) {
+            $user = new Users();
+            $user->setUsername($username);
+            $user->setLastname($lastname);
+            $user->setFirstname($firstname);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+            $user->setEmail($email);
+            $token = bin2hex(random_bytes(100));
+            $user->setToken($token);
+            $user->setRoles($roles);
 
-$manager->persist($user);
-$this->addReference($username, $user);
-}
+            $manager->persist($user);
+            $this->addReference($username, $user);
+        }
 
-$manager->flush();
-}
+        $manager->flush();
 
-private function getUserData(): array
-{
-return [
-// $userData = [$$username, $lastname, $firstname, $password, $email, $roles];
-['jane_admin', 'Doe', 'Jane', 'kitten', 'jane_admin@symfony.com', ['ROLE_ADMIN']],
-['tom_admin', 'Doe', 'Tom', 'kitten', 'tom_admin@symfony.com', ['ROLE_ADMIN']],
-['john_user', 'Doe', 'John', 'kitten', 'john_user@symfony.com', ['ROLE_USER']],
-];
-}
+        foreach ($this->getArtData() as [$title, $content, $goal, $status, $actived, $user_id]) {
+            $article = new Articles();
+            $article->setTitle($title);
+            $article->setContent($content);
+            $article->setGoal($goal);
+            $article->setStatus($status);
+            $article->setActived($actived);
+            $article->setUserId($user_id);
 
+            $manager->persist($article);
+            $this->addReference($title, $article);
+        }
 
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+        {
+        return [
+            // $userData = [$username, $lastname, $firstname, $password, $email, $roles];
+            ['jane_admin', 'Doe', 'Jane', 'kitten', 'jane_admin@symfony.com', ['ROLE_ADMIN']],
+            ['tom_admin', 'Doe', 'Tom', 'kitten', 'tom_admin@symfony.com', ['ROLE_ADMIN']],
+            ['john_user', 'Doe', 'John', 'kitten', 'john_user@symfony.com', ['ROLE_USER']],
+        ];
+    }
+
+    private function getArtData(): array
+    {
+        return [
+            // $userData = [$title, $content, $goal, $status, $actived, $user_id];
+            ['titre1', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1000, 0, 1, 1],
+            ['titre2', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 2500, 0, 0, 2],
+            ['titre3', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3000, 0, 1,3],
+            ['titre4', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 5000, 0, 0, 1],
+            ['titre5', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 100, 0, 1, 1],
+            ['titre6', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1200, 0, 0, 2],
+            ['titre7', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 300, 0, 1, 1],
+            ['titre8', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 15000, 0, 1, 3],
+
+        ];
+    }
 }
