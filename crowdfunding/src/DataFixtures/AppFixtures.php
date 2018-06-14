a3a6f4369cb2d02004bf78778987a3eea25c80cb
sanji69 <axel.aubry69@gmail.com>
2018-06-14 09:51:58 +0200

    fixture+bdd

--- crowdfunding/src/DataFixtures/AppFixtures.php
+++ crowdfunding/src/DataFixtures/AppFixtures.php
@@ -1,19 +1,24 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Sanji
+ * Date: 13/06/2018
+ * Time: 10:09
+ */
 
 namespace App\DataFixtures;
 
-use App\Entity\Contributor;
 use App\Entity\Users;
-USE App\Entity\Articles;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Articles;
+use App\Entity\Contributor;
+use \DateTime;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-//use Symfony\Component\Validator\Constraints\DateTime;
 
-use \DateTime;
-
-class AppFixtures extends Fixture
+class AppFixtures extends AbstractFixture
 {
+
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
@@ -21,8 +26,20 @@ class AppFixtures extends Fixture
         $this->passwordEncoder = $passwordEncoder;
     }
 
+
     public function load(ObjectManager $manager)
     {
+        loadUser();
+        loadArt();
+        loadContrib();
+    }
+
+    /*
+     * Users Fixtures
+     */
+    public function loadUser(ObjectManager $manager)
+    {
+        //chargement utilisateurs
         foreach ($this->getUserData() as [$username, $lastname, $firstname, $password, $email, $roles]) {
             $user = new Users();
             $user->setUsername($username);
@@ -30,7 +47,7 @@ class AppFixtures extends Fixture
             $user->setFirstname($firstname);
             $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
             $user->setEmail($email);
-            $user->setToken(rand(250,1000));
+            $user->setToken(rand(250, 1000));
             $user->setRoles($roles);
 
             $manager->persist($user);
@@ -38,8 +55,26 @@ class AppFixtures extends Fixture
         }
 
         $manager->flush();
+    }
 
-        foreach ($this->getArtData() as [$title, $content, $goal, $status, $actived, $user) {
+    private function getUserData(): array
+    {
+        return [
+            // $userData = [$username, $lastname, $firstname, $password, $email, $roles];
+            ['jane_admin', 'Doe', 'Jane', 'kitten', 'jane_admin@symfony.com', 'a:1:{i:0;s:10:"ROLE_ADMIN";}'],
+            ['tom_admin', 'Doe', 'Tom', 'kitten', 'tom_admin@symfony.com', 'a:1:{i:0;s:10:"ROLE_ADMIN";}'],
+            ['john_user', 'Doe', 'John', 'kitten', 'john_user@symfony.com', 'a:1:{i:0;s:9:"ROLE_USER";}'],
+        ];
+    }
+
+    /*
+     * Articles Fixtures
+     */
+    public function loadArt(ObjectManager $manager)
+    {
+
+        // chargement articles
+        foreach ($this->getArtData() as [$title, $content, $goal, $status, $actived, $user]) {
             $article = new Articles();
             $article->setTitle($title);
             $article->setContent($content);
@@ -53,8 +88,32 @@ class AppFixtures extends Fixture
         }
 
         $manager->flush();
+    }
 
-        foreach ($this->getContData() as [$users, $articles, $value]) {
+    private function getArtData(): array
+    {
+        return [
+            // $userData = [$title, $content, $goal, $status, $actived, $user];
+            ['titre1', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1000, 1000, 1, 1],
+            ['titre2', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 2500, 800, 0, 2],
+            ['titre3', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3000, 1200, 1,3],
+            ['titre4', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 5000, 1000, 0, 1],
+            ['titre5', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1000, 800, 1, 1],
+            ['titre6', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1200, 1200, 0, 2],
+            ['titre7', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3000, 1000, 1, 1],
+            ['titre8', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 15000, 800, 1, 3],
+
+        ];
+    }
+
+    /*
+     * Contribution Fixtures
+     */
+    public function loadContrib(ObjectManager $manager)
+    {
+
+        // chargement contributions
+        foreach ($this->getContribData() as [$users, $articles, $value]) {
             $contrib = new Contributor();
             $contrib->setUsers($users);
             $contrib->setArticles($articles);
@@ -68,33 +127,7 @@ class AppFixtures extends Fixture
         $manager->flush();
     }
 
-    private function getUserData(): array
-        {
-        return [
-            // $userData = [$username, $lastname, $firstname, $password, $email, $roles];
-            ['jane_admin', 'Doe', 'Jane', 'kitten', 'jane_admin@symfony.com', 'a:1:{i:0;s:10:"ROLE_ADMIN";}'],
-            ['tom_admin', 'Doe', 'Tom', 'kitten', 'tom_admin@symfony.com', 'a:1:{i:0;s:10:"ROLE_ADMIN";}'],
-            ['john_user', 'Doe', 'John', 'kitten', 'john_user@symfony.com', 'a:1:{i:0;s:9:"ROLE_USER";}'],
-        ];
-    }
-
-    private function getArtData(): array
-    {
-        return [
-            // $userData = [$title, $content, $goal, $status, $actived, $user_id];
-            ['titre1', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1000, 1000, 1, 1],
-            ['titre2', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 2500, 800, 0, 2],
-            ['titre3', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3000, 1200, 1,3],
-            ['titre4', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 5000, 1000, 0, 1],
-            ['titre5', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1000, 800, 1, 1],
-            ['titre6', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1200, 1200, 0, 2],
-            ['titre7', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 3000, 1000, 1, 1],
-            ['titre8', "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 15000, 800, 1, 3],
-
-        ];
-    }
-
-    private function getContData(): array
+    private function getContribData(): array
     {
         return [
             // $userData = [$users_id, $articles_id, $value];
@@ -117,4 +150,6 @@ class AppFixtures extends Fixture
             [1, 8, 400],
         ];
     }
+
+
 }
